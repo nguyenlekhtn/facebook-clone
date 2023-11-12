4c003e8f60e49a0d79380da616ee32e861b7620c
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-11-12 11:07:17 +0700

    add author reference column to Post table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_07_142906) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_12_035325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_07_142906) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_07_142906) do
     t.index ["username"], name: "index_users_on_username"
   end
 
+  add_foreign_key "posts", "users", column: "author_id"
 end
