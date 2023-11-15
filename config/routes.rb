7422fdd4a67e16441e3157a3aa6cb11706c67d47
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-11-15 20:22:08 +0700

    nest posts roues inside users routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'posts#index'
 
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :posts, only: %i[new create]
+  end
 end
