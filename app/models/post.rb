2664fd5a959a71bb337335e0ed808ab1e9066f94
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2024-01-06 12:40:15 +0700

    upgra rails version to 7.1

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  validates :content, presence: true
+  validates :body, presence: true
 end
